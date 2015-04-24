c05a5cebafce3889929cf8d0e76bbf481e9abaa8
fap <fap@gaia.gaia>
2015-04-24 21:55:51 +0200

    Fixes aws upload

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,12 +2,12 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
   config.aws_acl    = :public_read
-  config.region     = 'eu-central-1'
-  config.asset_host = 'https://seic-storage.s3-website.eu-central-1.amazonaws.com'
+  AWS.config(:region => 'eu-central-1')
+  config.asset_host = 'https://s3.eu-central-1.amazonaws.com/seic-storage'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 
   config.aws_credentials = {
-    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    access_key_id:  ENV.fetch('AWS_ACCESS_KEY_ID'),
     secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY')
   }
 end

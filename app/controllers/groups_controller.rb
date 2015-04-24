5da85580dc13a8bd5c873b74f0c6dc6d412a055c
fap <fap@gaia.gaia>
2015-04-24 16:00:59 +0200

    Adds link to group page in show_group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -2,6 +2,10 @@ class GroupsController < ApplicationController
   def new
   end
 
+  def show
+    @group = Group.find(params[:id])
+  end
+
   before_action :authenticate_user!
   def index
     @group_membership = User.group_membership(current_user)

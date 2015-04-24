56431b5009c671d86ababb647012342c689d5437
fap <fap@gaia.gaia>
2015-04-24 21:25:38 +0200

    Fix aws?

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,6 +2,7 @@ CarrierWave.configure do |config|
   config.storage    = :aws
   config.aws_bucket = ENV.fetch('S3_BUCKET_NAME')
   config.aws_acl    = :public_read
+  config.region     = 'eu-central-1'
   config.asset_host = 'https://seic-storage.s3-website.eu-central-1.amazonaws.com'
   config.aws_authenticated_url_expiration = 60 * 60 * 24 * 365
 

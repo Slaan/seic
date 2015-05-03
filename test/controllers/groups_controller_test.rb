9db469b77085a323897c24a9fceed968e610aa4b
fap <fap@gaia.gaia>
2015-05-03 17:20:10 +0200

    Works on groups controller test Groups controller test is failing because of required authentification

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class GroupsControllerTest < ActionController::TestCase
+
+  def setup
+    @user = users(:one)
+    @user.authenticate(default_password)
+  end
   test "should get new" do
     get :new
     assert_response :success

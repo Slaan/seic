165ca1d6b7d56a769f9f91e7aaa481bc067ee2c5
Timmay24 <tim.hartig@haw-hamburg.de>
2015-06-02 04:26:22 +0200

    Add deserialization for all tracks at once

--- lib/tracks_deserializer_mark.rb
+++ lib/tracks_deserializer_mark.rb
@@ -1,9 +1,16 @@
 class TracksDeserializerMark
 
   def self.deserialize(track)
-    Track.new(name: track["track_name"],
-      description: track["track_description"],
-      keywords: track["track_keywords"],
-      waypoints: track["track_geojson"])
+    Track.new("name" => track["track_name"],
+              "description" => track["track_description"],
+              "keywords" => track["track_keywords"],
+              "waypoints" => track["track_geojson"])
   end
+
+  def self.deserialize_all(tracks)
+    tracks.reduce([]) do |accu, track|
+      accu << self.deserialize(track)
+    end
+  end
+
 end

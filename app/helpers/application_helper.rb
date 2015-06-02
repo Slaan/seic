0e5004a7e3ae9dfdf65ea68e5e5da56c1d36b113
Timmay24 <tim.hartig@haw-hamburg.de>
2015-06-02 02:58:46 +0200

    Add thumbnail(model) method to refactor thumbnail insertion in member/group/event lists etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def thumbnail(model)
+    (model.picture?) ? (image_tag model.picture.url) : (missing_img_thumb)
+  end
+
+  def missing_img_thumb
+    image_tag 'https://s3.eu-central-1.amazonaws.com/seic-storage/public/images/missing_pic_thumb.png'
+  end
+
 end

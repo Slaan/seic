2a2a7b6415719ae1e557d17e3a7214467f52af48
fap <fap@gaia.gaia>
2015-06-13 18:46:11 +0200

    Moves connector functionality to own module

--- lib/connector_mark.rb
+++ lib/connector_mark.rb
@@ -26,67 +26,6 @@ class ConnectorMark
     end
   end
 
-  def setup_community_connection
-    setup_connection(COMMUNITY_NAME, COMMUNITY_PASSWORD)
-  end
-
-  def setup_connection(user_name, user_password)
-    Faraday.new API_URL, :ssl => {:verify => false} do |builder|
-      builder.headers[:token] = COMMUNITY_TOKEN
-      builder.use Faraday::Request::Retry
-      builder.request :json
-      builder.response :json, :content_type => /\bjson$/
-      builder.use :instrumentation
-      builder.use FaradayMiddleware::ParseJson, content_type: 'application/json'
-      builder.use Faraday::Response::Logger, Logger.new('faraday.log')
-      builder.use Faraday::Request::BasicAuthentication, user_name, user_password
-      builder.use Faraday::Response::Logger
-      builder.adapter Faraday.default_adapter
-    end
-  end
-
-  def connection(user)
-    connection = setup_connection(user.username, user.password_digest)
-    ConnectorMark.new(connection)
-  end
-
-  def connection(username, password)
-    connection = setup_connection(username, password)
-    ConnectorMark.new(connection)
-  end
-
-  def login(connection)
-    connection.get('communities/' + COMMUNITY_NAME.downcase).body["token"]
-  end
-
-  # Used to retrieve existing records
-  def get(path, params = nil)
-    @connection.get(path) do |request|
-      request.params = params if params
-    end
-  end
-
-  # Used to create new records
-  def post(path, params = nil)
-    @connection.post(path) do |request|
-      request.body = JSON.generate(params) if params.kind_of? Hash
-      request.body = params.to_json if params.kind_of? ActiveRecord::Base
-      p "Posting body: #{request.body}"
-    end
-  end
-
-  # Used to update existing records
-  def put(path, params = nil)
-    @connection.put(path) do |request|
-      request.body = JSON.generate(params) if params
-      p "Put body: #{request.body}"
-    end
-  end
-
-  def delete(path)
-    @connection.delete(path)
-  end
-
   module ApiMethods
 
     TRACKS_PATH = "tracks"
@@ -168,6 +107,7 @@ class ConnectorMark
     end
   end
 
+  include Connector
   include ApiMethods
 end
 

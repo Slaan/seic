adaf6f5506e55d7366f01f05ed09e5bed0f794f3
Isaisaisa <louisa@spahl-hamburg.de>
2015-06-13 21:07:07 +0200

    add function "get" to trackController

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -1,10 +1,8 @@
 class TracksController < ApplicationController
-
   CONNECTOR = ConnectorFactory.connection
 
   def index
     @tracks = TracksDeserializerMark.deserialize_all(CONNECTOR.connection(current_user).get_tracks_of(current_user).body)
-
   end
 
   def show
@@ -27,6 +25,19 @@ class TracksController < ApplicationController
   end
 
   def edit
+  end
+
+  def get
+    id1 = params[:id]
+    id = id1.split("\"")[1]
+    p id
+    @tracks = TracksDeserializerMark.deserialize_all(CONNECTOR.connection(current_user).get_tracks_of(current_user).body)
+    p @tracks
+
+    @track = TracksDeserializerMark.deserialize(CONNECTOR.connection(current_user).get_track(id).body)
+    # puts "@track"
+    # p @track
+
   end
 
 end

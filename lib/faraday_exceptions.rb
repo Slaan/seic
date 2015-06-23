e4c6c963bdb680c3862377ca08234b3f2490daa9
fap <fap@email.de>
2015-06-23 16:33:26 +0200

    Rescue TimeoutError in faraday middleware

--- lib/faraday_exceptions.rb
+++ lib/faraday_exceptions.rb
@@ -1,13 +1,12 @@
   class FaradayExceptions < Faraday::Middleware
-    
+
     def call(env)
       begin
         @app.call(env)
-      rescue Faraday::Error::ConnectionFailed => e
+      rescue Faraday::Error::ConnectionFailed, Faraday::Error::TimeoutError => e
         url = env[:url].to_s.gsub(env[:url].path, '')
         $stderr.puts "The server at #{url} is either unavailable or is not currently accepting requests. Please try again in a few minutes."
       end
     end
-    
-  end
 
+  end
